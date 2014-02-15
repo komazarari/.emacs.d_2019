8a1304a6b085c9a8f14d9360db8df3d782e7df57
Takuto Komazaki <komazarari@gmail.com>
2014-02-15 13:47:44 +0900

    init

---
+++ packages.el
@@ -0,0 +1,24 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;; package.el
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elisp/elpa/")
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
+(package-initialize)
+
+(pkg-sync 'exec-path-from-shell require
+(exec-path-from-shell-initialize))
+
+;;; el-get
+(add-to-list 'load-path (locate-user-emacs-file "./.emacs.d/elisp/el-get"))
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (let (el-get-master-branch)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))))
+(require 'el-get nil t)
+(add-to-list 'el-get-recipe-path (locate-user-emacs-file "./recipes"))
+
+(el-get 'sync)
