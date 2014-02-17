aeb1a21a8f12d532af443b4135ee330cafaa30dc
takuto.komazaki <komazarari@gmail.com>
2014-02-17 15:55:24 +0900

    OSX font setting

--- inits/11_font.el
+++ inits/11_font.el
@@ -2,3 +2,9 @@
   (set-face-attribute 'default nil
                       :family "Ricty"
                       :height 160))
+
+;; Mac では日本語をヒラギノ丸ゴProNに
+(when (eq system-type 'darwin)
+  (set-fontset-font "fontset-default"
+                    'japanese-jisx0208
+                    '("Hiragino Maru Gothic ProN")))
