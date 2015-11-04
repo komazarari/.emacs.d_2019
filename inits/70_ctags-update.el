12eabfba111b3870b4363d6edc33981bae630a26
takuto.komazaki <takuto.komazaki@gree.net>
2015-11-04 13:57:13 +0900

    Ctags update

--- inits/70_ctags-update.el
+++ inits/70_ctags-update.el
@@ -1,4 +1,7 @@
 (require 'ctags nil t)
+(exec-path-from-shell-initialize)
+(setq ctags-command "/usr/local/bin/ctags -e -R ")
+
 (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
 (add-hook 'c-mode-common-hook  'turn-on-ctags-auto-update-mode)
 (add-hook 'ruby-mode-hook  'turn-on-ctags-auto-update-mode)
